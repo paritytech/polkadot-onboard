aadf1429c9d515fd0a821b2f26038aa700cadbfd
hamidra <hamid.alipour@gmail.com>
2022-08-19 11:41:14 -0700

    fix: add missing types

--- packages/react/src/contexts/DotsamaWalletsContext.tsx
+++ packages/react/src/contexts/DotsamaWalletsContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useMemo, useCallback } from 'react';
-import { getExtensions, ExtensionInfo, BasicExtensionInfo } from '@dotsama-wallets/core';
+import { getExtensions, DotsamaWallet, DotsamaWalletBasic, ExtensionEnabler } from '@dotsama-wallets/core';
 import { Injected, InjectedAccount } from '@polkadot/extension-inject/types';
 
 interface DotsamaWalletsContextProviderProps {
@@ -8,31 +8,34 @@ interface DotsamaWalletsContextProviderProps {
 
 interface DotsamaWalletsContextProps {
   setupWallets: () => void;
-  connectToExtension: (appName: string, extension: ExtensionInfo) => Promise<{ injectedExtension: Injected, accounts: InjectedAccount[] }>;
-  extensions: ExtensionInfo[];
-  otherExtensions: BasicExtensionInfo[];
+  connectToExtension: (appName: string, extension: DotsamaWallet<ExtensionEnabler>) => Promise<{ injectedExtension: Injected; accounts: InjectedAccount[] }>;
+  extensions: DotsamaWallet<ExtensionEnabler>[];
+  otherExtensions: DotsamaWalletBasic<ExtensionEnabler>[];
 }
 
 export const DotsamaWalletsContext = createContext<DotsamaWalletsContextProps>({
   setupWallets: () => {},
-  connectToExtension: async () => ({ injectedExtension: {} as Injected, accounts: []}),
+  connectToExtension: async () => ({ injectedExtension: {} as Injected, accounts: [] }),
   extensions: [],
   otherExtensions: [],
 });
 
 export const DotsamaWalletsContextProvider = ({ children }: DotsamaWalletsContextProviderProps) => {
-  const [extensions, setExtensions] = useState<ExtensionInfo[]>([]);
-  const [otherExtensions, setOtherExtensions] = useState<BasicExtensionInfo[]>([]);
+  const [extensions, setExtensions] = useState<DotsamaWallet<ExtensionEnabler>[]>([]);
+  const [otherExtensions, setOtherExtensions] = useState<DotsamaWalletBasic<ExtensionEnabler>[]>([]);
 
-  const connectToExtension = useCallback(async (appName: string, extension: ExtensionInfo) => {
-    // TODO extension connection should be separate from getting accounts, but for that will need ping functionality to get isConnected status
-    // currently we don't have that yet
-    // add try catches
-    const injectedExtension = await extension.enable(appName);
-    const accounts = await injectedExtension.accounts.get();
+  const connectToExtension = useCallback(
+    async (appName: string, extension: DotsamaWallet<ExtensionEnabler>) => {
+      // TODO extension connection should be separate from getting accounts, but for that will need ping functionality to get isConnected status
+      // currently we don't have that yet
+      // add try catches
+      const injectedExtension = await extension.enable(appName);
+      const accounts = await injectedExtension.accounts.get();
 
-    return { injectedExtension, accounts };
-  }, [extensions, otherExtensions]);
+      return { injectedExtension, accounts };
+    },
+    [extensions, otherExtensions],
+  );
 
   // initializer, gets all the extensions
   const setupWallets = () => {
@@ -41,7 +44,7 @@ export const DotsamaWalletsContextProvider = ({ children }: DotsamaWalletsContex
 
     setExtensions([...knownExtensions]); // TODO: potentially can be [...knownExtensions, walletConnect]
     setOtherExtensions([...otherExtensions]);
-  }
+  };
 
   const contextData = useMemo(
     () => ({
