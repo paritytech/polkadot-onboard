ded3d96f84eb709e09307a0ba6c186f80b11724b
hamidra <hamid.alipour@gmail.com>
2022-09-19 18:59:03 -0700

    fix: remove debug leftovers

--- examples/react-dapp/next.config.js
+++ examples/react-dapp/next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 
 const isProd = process.env.NODE_ENV === 'production';
-console.log(`isProd: ${isProd}`);
 const nextConfig = {
   assetPrefix: isProd ? '/wallet-aggregator-sdk/' : '',
   reactStrictMode: true,
