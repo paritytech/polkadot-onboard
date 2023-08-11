c2024ccce8e8fa6c2e5c868158762b3221c9fa0f
Vadim <vadim.sav@gmail.com>
2023-08-11 18:18:01 +0300

    Update packages/wallet-connect/src/types.ts
    Co-authored-by: Hamid Alipour <hamid.alipour@gmail.com>

--- packages/wallet-connect/src/types.ts
+++ packages/wallet-connect/src/types.ts
@@ -5,6 +5,8 @@ export type WcAccount = `${string}:${string}:${string}`;
 export type PolkadotNamespaceChainId = `polkadot:${string}`;
 
 export interface WalletConnectConfiguration extends SignClientTypes.Options {
+  // ToDo: Remove ```projectId``` when the following issue is resolved:
+  // https://github.com/WalletConnect/walletconnect-monorepo/pull/3435
   projectId: string;
   chainIds?: PolkadotNamespaceChainId[];
   optionalChainIds?: PolkadotNamespaceChainId[];
