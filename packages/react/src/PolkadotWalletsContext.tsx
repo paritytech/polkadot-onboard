427829c19fce1b8cab93f73ca322777c786d8797
saltycucumber <vadim.sav@gmail.com>
2023-08-30 18:49:19 +0300

    restored 5 ms delay in wallets context

--- packages/react/src/PolkadotWalletsContext.tsx
+++ packages/react/src/PolkadotWalletsContext.tsx
@@ -5,6 +5,7 @@ import { createContext, useState, useMemo, useEffect, useContext } from 'react';
 interface PolkadotWalletsContextProviderProps {
   children: any;
   walletAggregator: WalletAggregator;
+  initialWaitMs?: number;
 }
 
 interface PolkadotWalletsContextProps {
@@ -17,17 +18,21 @@ const PolkadotWalletsContext = createContext<PolkadotWalletsContextProps>({
 
 export const useWallets = () => useContext(PolkadotWalletsContext);
 
-export const PolkadotWalletsContextProvider = ({ children, walletAggregator }: PolkadotWalletsContextProviderProps) => {
+export const PolkadotWalletsContextProvider = ({
+  children,
+  walletAggregator,
+  initialWaitMs = 5 /* the default is set to 5ms to give extensions enough lead time to inject their providers */,
+}: PolkadotWalletsContextProviderProps) => {
   const [wallets, setWallets] = useState<BaseWallet[] | undefined>();
 
   useEffect(() => {
-    const getWallets = async () => {
+    const timeoutId = setTimeout(async () => {
       const wallets = await walletAggregator.getWallets();
       setWallets(wallets);
-    };
+    }, initialWaitMs);
 
-    getWallets();
-  }, []);
+    return () => clearTimeout(timeoutId);
+  }, [walletAggregator]);
 
   const contextData = useMemo(
     () => ({
